afaa6447b64c82f006e3c1f7f836d80684bfd5e6
gmarkomanolis <george@markomanolis.com>
2017-06-29 15:05:26 +0800

    fixing a conflict of a file

--- proposal-draft/kaust-georgios/scheduler.sh
+++ proposal-draft/kaust-georgios/scheduler.sh
@@ -5,6 +5,8 @@ if [ $1 -eq 1 ]; then
 echo -e "#SBATCH --ntasks-per-node=
 #SBATCH --nodes=
 #SBATCH --job-name=IO-500
+#SBATCH -A 
+#SBATCH --partition=
 #SBATCH --time=
 #SBATCH -o io_500_out_%J
 #SBATCH -e io_500_err_%J\n" >> $2
@@ -12,7 +14,19 @@ echo -e "#SBATCH --ntasks-per-node=
 elif [ $1 -eq 2 ]; then
 echo -e "#PBS -l select=:mpiprocs=
 #PBS -N IO-500
+#PBS -A 
+#PBS -q 
 #PBS -l walltime=
 #PBS -o io_500_out
 #PBS -e io_500_err" >> $2
+
+elif [ $1 -eq 3 ]; then
+echo -e "#BSUB -n
+#BSUB -R \"span[ptile=]\"
+#BSUB -J IO-500
+#BSUB -P
+#BSUB -q 
+#BSUB -W
+#BSUB -o io_500_out_%J
+#BSUB -e io_500_err_%J" >> $2
 fi
