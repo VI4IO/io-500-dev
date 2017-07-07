535e7e7610f8a229448c489045183b936b437f15
Julian M. Kunkel <Julian.Kunkel@googlemail.com>
2017-07-07 18:47:19 +0200

    Bugfix find on root dir.

--- proposal-draft/dkrz/io500-find.sh
+++ proposal-draft/dkrz/io500-find.sh
@@ -2,10 +2,11 @@
 # feel free to replace this command with another command
 # Input arguments is the workdir
 # Expected output is the number of found entities divided by the runtime, e.g., ioops for the found entities
-workdir=$1
+timestamp=$1
+workdir=$2
 
 start=$(date +%s.%N)
-found=$(find ${workdir} -name \*.0.\* -newer ${workdir}/timestamp  -size +3000c | wc -l)
+found=$(find ${workdir} -name \*.0.\* -newer $timestamp  -size +3000c | wc -l)
 end=$(date +%s.%N)
 export duration=$(echo "scale=2; $end - $start" | bc)
 
