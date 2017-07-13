a1a33f04a9ec075ddb11f14bc7f1101b80ff728c
Julian M. Kunkel <juliankunkel@googlemail.com>
2017-07-13 15:28:59 +0200

    Updated download to mdtest.

--- proposal-draft/prepare.sh
+++ proposal-draft/prepare.sh
@@ -6,6 +6,6 @@ mkdir download 2>/dev/null
 
 cd download
 git clone https://github.com/LLNL/mdtest.git
-git clone https://github.com/LLNL/ior.git
+git clone https://github.com/IOR-LANL/ior
 git clone https://github.com/JulianKunkel/md-real-io
 
