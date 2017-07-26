b59bb6aa85f810fbe8c5e7fab95ef60368dc1e6f
George Markomanolis <georgios.markomanolis@kaust.edu.sa>
2017-07-26 13:54:06 +0300

    the startup file to use when you know the optimum values, need to add the values in the script

---
+++ proposal-draft/kaust-georgios/startup-datawarp-bb.sh
@@ -0,0 +1,51 @@
+#!/bin/bash 
+#SBATCH --ntasks-per-node=8
+#SBATCH --partition=workq
+#SBATCH --nodes=100
+#SBATCH --job-name=IO-500
+#SBATCH --time=02:30:00
+#SBATCH -o io_500_out_%J
+#SBATCH -e io_500_err_%J
+
+#DW jobdw type=scratch access_mode=striped capacity=3200GiB
+
+# parameters that are always true
+# If hyperthreading is not active, do not divide by two in the next command
+let maxTasks=$((${SLURM_JOB_CPUS_PER_NODE} * ${SLURM_JOB_NUM_NODES}))/2
+mpirun="srun -m block"
+workdir=/$DW_JOB_STRIPED/test.$$/
+output_dir=/$DW_JOB_STRIPED/io500-results-${SLURM_JOB_NUM_NODES}
+
+#params_mdreal="-P=5000 -I=1000"
+  subtree_to_scan_config=$PWD/subtree.cfg
+  
+  # The subtrees to scan from md-easy, each contains mdtest_easy_files_per_proc files
+  ( for I in $(seq 100) ; do 
+    echo mdtest_tree.$I.0
+  done ) > subtree.cfg
+
+# ToDo add here optimum values
+#ior_easy_params=
+#ior_hard_writes_per_proc=               
+#mdtest_hard_files_per_proc=
+#mdtest_easy_files_per_proc=
+
+# precreate directories for lustre with the appropriate striping
+mkdir -p ${workdir}/ior_easy
+
+mkdir -p ${workdir}/ior_hard
+
+# commands
+find_cmd=$PWD/../io500-find.sh
+ior_cmd=/project/k01/markomg/burst_test/BB_ior/io-500-dev/proposal-draft/ior
+mdtest_cmd=/project/k01/markomg/burst_test/BB_ior/io-500-dev/proposal-draft/mdtest
+mdreal_cmd=/project/k01/markomg/burst_test/BB_ior/io-500-dev/proposal-draft/md-real-io # set to "" to not run mdreal
+
+params_mdreal="-P=10 -I=10"
+
+(
+cd ..
+source io_500_core.sh
+) 2>&1 | tee $SLURM_NNODES.txt
+
+rm -rf $workdir/
