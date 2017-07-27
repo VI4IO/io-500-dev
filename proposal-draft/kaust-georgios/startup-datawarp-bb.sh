4cb1391d03626dee5fa5e0429e9422c4012b4644
George Markomanolis <georgios.markomanolis@kaust.edu.sa>
2017-07-27 22:26:56 +0300

    adding tuned parameters for DataWarp

--- proposal-draft/kaust-georgios/startup-datawarp-bb.sh
+++ proposal-draft/kaust-georgios/startup-datawarp-bb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 #SBATCH --ntasks-per-node=8
 #SBATCH --partition=workq
-#SBATCH --nodes=100
+#SBATCH --nodes=300
 #SBATCH --job-name=IO-500
 #SBATCH --time=02:30:00
 #SBATCH -o io_500_out_%J
@@ -11,10 +11,10 @@
 
 # parameters that are always true
 # If hyperthreading is not active, do not divide by two in the next command
-let maxTasks=$((${SLURM_JOB_CPUS_PER_NODE} * ${SLURM_JOB_NUM_NODES}))/2
+let maxTasks=$((${SLURM_NTASKS_PER_NODE} * ${SLURM_JOB_NUM_NODES}))/2
 mpirun="srun -m block"
 workdir=/$DW_JOB_STRIPED/test.$$/
-output_dir=/$DW_JOB_STRIPED/io500-results-${SLURM_JOB_NUM_NODES}
+output_dir=/project/k01/markomg/bb_io500-results-${SLURM_JOB_NUM_NODES}
 
 #params_mdreal="-P=5000 -I=1000"
   subtree_to_scan_config=$PWD/subtree.cfg
@@ -25,10 +25,10 @@ output_dir=/$DW_JOB_STRIPED/io500-results-${SLURM_JOB_NUM_NODES}
   done ) > subtree.cfg
 
 # ToDo add here optimum values
-#ior_easy_params=
-#ior_hard_writes_per_proc=               
-#mdtest_hard_files_per_proc=
-#mdtest_easy_files_per_proc=
+ior_easy_params="-t 2m -b 192616m"
+ior_hard_writes_per_proc=77872
+mdtest_hard_files_per_proc=1630
+mdtest_easy_files_per_proc=10800
 
 # precreate directories for lustre with the appropriate striping
 mkdir -p ${workdir}/ior_easy
@@ -41,7 +41,7 @@ ior_cmd=/project/k01/markomg/burst_test/BB_ior/io-500-dev/proposal-draft/ior
 mdtest_cmd=/project/k01/markomg/burst_test/BB_ior/io-500-dev/proposal-draft/mdtest
 mdreal_cmd=/project/k01/markomg/burst_test/BB_ior/io-500-dev/proposal-draft/md-real-io # set to "" to not run mdreal
 
-params_mdreal="-P=10 -I=10"
+params_mdreal="-P=1000 -I=100"
 
 (
 cd ..
