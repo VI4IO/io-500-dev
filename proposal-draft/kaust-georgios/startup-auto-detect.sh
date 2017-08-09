61ee8377f29725bdabf322a4da96a140f4fe0e04
George Markomanolis <georgios.markomanolis@kaust.edu.sa>
2017-08-09 15:39:33 +0300

    tuning script for Lustre

---
+++ proposal-draft/kaust-georgios/startup-auto-detect.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e
+#SBATCH --ntasks-per-node=16
+#SBATCH --partition=workq
+#SBATCH --nodes=1000
+#SBATCH --job-name=IO-500
+#SBATCH --time=06:30:00
+#SBATCH -o io_500_out_%J
+#SBATCH -e io_500_err_%J
+
+
+# parameters that are always true
+# If hyperthreading is not active, do not divide by two in the next command
+let maxTasks=$((${SLURM_NTASKS_PER_NODE} * ${SLURM_JOB_NUM_NODES}))/2
+mpirun="srun -m block --hint=nomultithread"
+workdir=/project/k01/markomg/test.$$/
+output_dir=/project/k01/markomg/io500-results-${SLURM_JOB_NUM_NODES}.$$
+
+# precreate directories for lustre with the appropriate striping
+mkdir -p ${workdir}/ior_easy
+lfs setstripe --stripe-count 2  ${workdir}/ior_easy
+
+mkdir -p ${workdir}/ior_hard
+lfs setstripe -c -1  ${workdir}/ior_hard
+
+# commands
+find_cmd=$PWD/../io500-find.sh
+ior_cmd=/project/k01/markomg/burst_test/BB_ior/io-500-dev/proposal-draft/ior
+mdtest_cmd=/project/k01/markomg/burst_test/BB_ior/io-500-dev/proposal-draft/mdtest
+#mdreal_cmd=/project/k01/markomg/burst_test/BB_ior/io-500-dev/proposal-draft/md-real-io # set to "" to not run mdreal
+mdreal_cmd="" # set to "" to not run mdreal
+
+params_mdreal="-P=1000 -I=100"
+
+#
+identify_parameters_ior_hard=True
+identify_parameters_ior_easy=True
+identify_parameters_md_easy=True # also enables to do the find 
+identify_parameters_md_hard=True
+identify_parameters_find=True # only works if md_easy is also run
+
+timeExpected=300 
+timeThreshhold=150
+cd ..
+source ./auto-determine-parameters.sh | tee auto-${SLURM_JOB_NUM_NODES}-${SLURM_NTASKS_PER_NODE}.txt 
