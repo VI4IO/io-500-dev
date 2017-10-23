65cc7ba60446afbcc05276e906bd60d4aa98bcb8
John Bent <johnbent@gmail.com>
2017-10-22 18:25:29 -0600

    Made the find command accept the 01 as a flag and not hard-coded. Also added tmp vim files to .gitignore

--- utilities/find/pfind.sh
+++ utilities/find/pfind.sh
@@ -3,6 +3,7 @@
 searchdir=$1
 timestamp_file=$2
 target_size=$3
+target_string=$4
 
 # also an environment variable is set containing $IO500_MPI
 # which contains any values for $io500_mpirun and $io500_mpiargs set in io500.sh
@@ -10,12 +11,10 @@ target_size=$3
 # feel free to edit this file as much as you want (e.g. change how MPI is launched).
 # if you do so, please just document and report the change in your submission.
 
-timestamp=`stat -c %Y $timestamp_file`  # take the target file and get the mtime from it
-
 pfind=`dirname $0`/pwalk/pfind          # ensure that the git repo has been cloned
 
 if [ -x "$pfind" ] ; then
-  $IO500_MPI $pfind -newer $timestamp -name "01" -size $target_size -silent $searchdir 
+  $IO500_MPI $pfind -newer $timestamp_file -name "$target_string" -size $target_size -silent $searchdir 
 else
   echo "ERROR: Need to install pfind.py from pwalk github repo"
 fi
