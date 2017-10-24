fb0f24849e06603133d74d04447fbbb913acb84a
John Bent <johnbent@gmail.com>
2017-10-24 11:10:15 -0600

    bug in path

--- utilities/find/pfind.sh
+++ utilities/find/pfind.sh
@@ -11,7 +11,7 @@ target_string=$4
 # feel free to edit this file as much as you want (e.g. change how MPI is launched).
 # if you do so, please just document and report the change in your submission.
 
-pfind=`dirname $0`/pwalk/pfind          # ensure that the git repo has been cloned
+pfind=`dirname $0`/pfind          # ensure that the git repo has been cloned
 
 if [ -x "$pfind" ] ; then
   $IO500_MPI $pfind -newer $timestamp_file -name "$target_string" -size $target_size -silent $searchdir 
